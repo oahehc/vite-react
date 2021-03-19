b05593bb7ab547ca740669fb6fa0092dc60de9d6
oahehc <oahehc@gmail.com>
2021-03-19 17:21:19 +0900

    conditon config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
 import { defineConfig } from "vite";
 import reactRefresh from "@vitejs/plugin-react-refresh";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [reactRefresh()],
-  esbuild: {
-    jsxInject: `import React from 'react'`,
-  },
-});
+export default ({ command, mode }) => {
+  if (command === "dev") {
+    return defineConfig({
+      plugins: [reactRefresh()],
+      esbuild: {
+        jsxInject: `import React from 'react'`,
+      },
+    });
+  } else {
+    return defineConfig({
+      plugins: [reactRefresh()],
+      esbuild: {
+        jsxInject: `import React from 'react'`,
+      },
+    });
+  }
+};
